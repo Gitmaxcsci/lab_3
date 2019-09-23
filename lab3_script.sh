319be9745d9965bc8fb137c572482f1aa9802c98
Gitmaxcsci <max.shetterly@colorado.edu>
2019-09-22 19:12:00 -0600

    default

--- lab3_script.sh
+++ lab3_script.sh
@@ -11,7 +11,7 @@ grep $regEx $fileN
 #3
 grep -E -c '\([0-9]\{3\})[ -]\?\)\{2\}[0-9]\{4\}' "regex_practice.txt"
 #4.1
-grep -E -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" "regex_practice.txt"
+grep -E -c "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\." "regex_practice.txt"
 #4.2
 grep -E -o '\([303][ -]\?\)\{2\}[0-9]\{4\}' "regex_practice.txt"
 #4.3
